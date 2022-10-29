7ae54abc50cabdc08c067d73acf39b4c9ef954fe
Tommy Tran <haitran1995@gmail.com>
2022-10-28 22:35:10 -0400

    add more notes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catsRouter = require('./routes/cats')
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,11 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Get data array from this route
-app.get('/cats', (req, res) => {
-  const cats = ['Hello Kitty', 'Garfield', 'Frits']
-  // ^ This would be our databass call...
-  res.json(cats);
-});
+app.use('/cats', catsRouter)
 
 module.exports = app;
