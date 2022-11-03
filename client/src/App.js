46aceaa3e52190b192aa33ee57d8a73614210e07
Tommy Tran <haitran1995@gmail.com>
2022-11-03 10:28:00 -0400

    remove unuse code

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,7 @@ import Login from './Login'
 import Register from './Register'
 
 function App() {
-  const [cats, setCats] = useState([]);
   const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios.get('/cats').then(res => {
-      console.log(res.data);
-      setCats(res.data);
-    })
-  }, [])
 
   useEffect(() => {
     axios.get('/users').then(res => {
@@ -22,13 +14,9 @@ function App() {
     })
   }, [])
 
-
-  // <h1>Wish List App</h1>
-  // {cats.length ? cats.map((cat, i) => <li key={i}>{cat}</li>) : <h2>Loading</h2>}
-
   return (
     <div className='App'>
-      <Register />
+      {/* <Register /> */}
       {/* {users.length ? users.map((user, y) => <li key={y}>{user}</li>) : <h2>Loading</h2>} */}
 
     </div>
